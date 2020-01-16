2a9696ffbd57adbb7b610ba215a8c81bc02e6c89
ymnai <1172217137@qq.com>
2020-01-16 13:36:24 +0800

    feat:添加文件run.sh

---
+++ run.sh
@@ -0,0 +1,36 @@
+#操作/项目路径(Dockerfile存放的路径)
+BASE_PATH=/var/lib/jenkins/workspace/springboot-helloworld
+# 源jar路径
+SOURCE_PATH=/var/lib/jenkins/workspace/springboot-helloworld/target
+#容器id
+CID=$(docker ps | grep "hello" | awk '{print $1}')
+
+DATE=`date +%Y%m%d%H%M`
+
+# 构建docker镜像
+function build(){
+	echo "开始构建镜像..."
+	cd $BASE_PATH
+	sudo docker build -t hello .
+}
+
+# 运行docker容器
+function run(){
+	backup
+	transfer
+	build
+	if [ -n "$CID" ]; then
+		echo "存在hello容器，CID=$CID,重启docker容器 ..."
+			sudo docker stop $CID
+			sudo docker rm $CID
+			sudo docker run -d -p 8085:8085 hello
+		echo "hello容器重启完成"
+	else
+		echo "不存在hello容器，docker run创建容器..."
+			sudo docker run -d -p 8085:8085 hello
+		echo "hello容器创建完成"
+	fi
+}
+
+#入口
+run
