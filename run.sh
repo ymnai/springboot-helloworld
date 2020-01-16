0ac2594cd1459ed38048709ac087605d3f39691e
ymnai <1172217137@qq.com>
2020-01-16 14:35:02 +0800

    feat:修改run.sh文件

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ DATE=`date +%Y%m%d%H%M`
 function build(){
 	echo "开始构建镜像..."
 	cd $BASE_PATH
-	sudo docker build -t hello .
+	docker build -t hello .
 }
 
 # 运行docker容器
@@ -21,13 +21,13 @@ function run(){
 	build
 	if [ -n "$CID" ]; then
 		echo "存在hello容器，CID=$CID,重启docker容器 ..."
-			sudo docker stop $CID
-			sudo docker rm $CID
-			sudo docker run -d -p 8085:8085 hello
+			 docker stop $CID
+			 docker rm $CID
+			 docker run -d -p 8085:8085 hello
 		echo "hello容器重启完成"
 	else
 		echo "不存在hello容器，docker run创建容器..."
-			sudo docker run -d -p 8085:8085 hello
+			 docker run -d -p 8085:8085 hello
 		echo "hello容器创建完成"
 	fi
 }
