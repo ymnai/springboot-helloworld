50083abfe30596bce1e6c357ea44a225c2d39809
ymnai <1172217137@qq.com>
2020-01-17 11:11:22 +0800

    feat:更改构建环境

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #操作/项目路径(Dockerfile存放的路径)
-BASE_PATH=/home/work/springboot-helloworld
-
+BASE_PATH=/var/lib/jenkins/workspace/springboot-helloworld
+# 源jar路径
+SOURCE_PATH=/var/lib/jenkins/workspace/springboot-helloworld/target
 #容器id
-CID=$(docker ps | grep "helloworld" | awk '{print $1}')
+CID=$(docker ps | grep "hello" | awk '{print $1}')
 
 DATE=`date +%Y%m%d%H%M`
 
@@ -10,24 +11,22 @@ DATE=`date +%Y%m%d%H%M`
 function build(){
 	echo "开始构建镜像..."
 	cd $BASE_PATH
-	docker build -t helloworld .
+	docker build -t hello .
 }
 
 # 运行docker容器
 function run(){
 	build
 	if [ -n "$CID" ]; then
-		echo "存在helloworld容器，CID=$CID,重启docker容器 ..."
+		echo "存在hello容器，CID=$CID,重启docker容器 ..."
 			docker stop $CID
 			docker rm $CID
-			docker run -d -p 12000:12000 helloworld \
-			-H unix:///var/run/docker.sock
-		echo "helloworld容器重启完成"
+			docker run -d -p 8085:8085 --name=helloWorld hello
+		echo "hello容器重启完成"
 	else
-		echo "不存在helloworld容器，docker run创建容器..."
-			docker run -d -p 12000:12000 helloworld \
-			-H unix:///var/run/docker.sock
-		echo "helloworld容器创建完成"
+		echo "不存在hello容器，docker run创建容器..."
+			docker run -d -p 8085:8085 --name=helloWorld hello
+		echo "hello容器创建完成"
 	fi
 }
 
