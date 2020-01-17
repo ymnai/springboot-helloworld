f64cb953529da5a304f01d3a0d891c409d5bd177
ymnai <1172217137@qq.com>
2020-01-17 09:35:18 +0800

    feat:run.sh修改映射端口

--- run.sh
+++ run.sh
@@ -21,12 +21,12 @@ function run(){
 		echo "存在hello容器，CID=$CID,重启docker容器 ..."
 			docker stop $CID
 			docker rm $CID
-			docker run -d -p 8080:8080 hello \
+			docker run -d hello -p 8080:8080 --name helloworld
 			-H unix:///var/run/docker.sock
 		echo "hello容器重启完成"
 	else
 		echo "不存在hello容器，docker run创建容器..."
-			docker run -d -p 8085:8085 hello \
+			docker run -d hello -p 8080:8080 --name helloworld
 			-H unix:///var/run/docker.sock
 		echo "hello容器创建完成"
 	fi
