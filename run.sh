1a70fbad5869624edb06833063588a787756bc54
ymnai <1172217137@qq.com>
2020-01-17 16:25:59 +0800

    feat:删除旧的镜像

--- run.sh
+++ run.sh
@@ -22,10 +22,10 @@ function run(){
 		echo "存在hello容器，CID=$CID,重启docker容器 ..."
 			docker stop $CID
 			docker rm $CID
+    echo "删除旧的hello镜像 ...$MID"
+            docker rmi $MID
 			docker run -d -p 8080:8080 hello
 		echo "hello容器重启完成"
-		echo "删除旧的的hello镜像 ..."
-		    docker rmi $MID
 	else
 		echo "不存在hello容器，docker run创建容器..."
 			docker run -d -p 8080:8080 hello
