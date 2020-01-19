99b714163fc8c818292af422a44f1360ca918050
ymnai <1172217137@qq.com>
2020-01-19 15:48:51 +0800

    feat:修改端口

--- src/main/java/com/gazgeek/helloworld/controller/HomeController.java
+++ src/main/java/com/gazgeek/helloworld/controller/HomeController.java
@@ -3,12 +3,14 @@ package com.gazgeek.helloworld.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class HomeController {
 
     @RequestMapping("/")
     String home() {
-        return "Hello! 测试自动上传镜像到阿里云镜像仓库 2020-01-19 11:55:00？";
+        return "Hello! 测试自动上传镜像到阿里云镜像仓库 ？"+ new Date();
     }
 
 }
